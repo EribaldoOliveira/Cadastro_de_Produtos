366b43496f3cb33aeb80bc5beec12f3fc0edc222
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-08-09 06:25:29 -0300

    Cadastro de produtos

---
+++ cadastro/domain/Produto.java
@@ -0,0 +1,9 @@
+package br.com.cadastro.domain;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Produto implements Serializable {
+
+}
---
+++ cadastro/repositories/ProdutoRepository.java
@@ -0,0 +1,5 @@
+package br.com.cadastro.repositories;
+
+public interface ProdutoRepository {
+
+}
