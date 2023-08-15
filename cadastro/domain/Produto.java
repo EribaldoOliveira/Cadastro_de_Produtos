21913b411b62a7adc2599704b7873cb0333f39ef
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-08-15 19:40:20 -0300

    Continuação do cadastro e teste

--- cadastro/domain/Produto.java
+++ cadastro/domain/Produto.java
@@ -6,4 +6,8 @@ import jakarta.persistence.Entity;
 @Entity
 public class Produto implements Serializable {
 
+	public Produto(Object object, String string, int i, double d) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
